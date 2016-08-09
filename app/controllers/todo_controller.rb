d61207cc99b262c9558225c7fb77aefbace73c0a
Austin Kabiru <austin.kabiru@andela.com>
2016-08-09 15:20:22 +0300

    feat(models): add models
    [Finishes #127424099 #127424073]

--- app/controllers/todo_controller.rb
+++ app/controllers/todo_controller.rb
@@ -1,4 +1,42 @@
 class TodoController < Panda::BaseController
   def index
+    @todos = Todo.all
+  end
+
+  def show
+    @todo = Todo.find(params["id"])
+  end
+
+  def new
+  end
+
+  def edit
+    @todo = Todo.find(params["id"])
+  end
+
+  def update
+    todo = Todo.find(params["id"])
+    todo.update(
+      title: params["title"],
+      body: params["body"],
+      status: params["status"]
+    )
+    redirect_to "/todo/#{todo.id}"
+  end
+
+  def create
+    Todo.create(
+      title: params["title"],
+      body: params["body"],
+      status: params["status"],
+      created_at: Time.now.to_s
+    )
+    redirect_to "/todo"
+  end
+
+  def destroy
+    todo = Todo.find(params["id"])
+    todo.destroy
+    redirect_to "/todo"
   end
 end
--- config.ru
+++ config.ru
@@ -1,5 +1,6 @@
+APP_ROOT = __dir__
 require "./config/application.rb"
-
+use Rack::MethodOverride
 TodoApplication = PandaTodo::Application.new
 
 require_relative "config/routes"
